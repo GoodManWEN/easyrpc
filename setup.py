b777f4e7e4856aeb796a6bd0cb863bff428d98fb
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2019-06-14 21:53:34 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,1 +1,36 @@
-# Nothing yet
+#############################################
+# File Name: setup.py
+# Author: WEN
+# Created Time:  2019-06-08 08:11:00
+#############################################
+
+from setuptools import setup
+
+setup(
+    name        =   "easyrpc",
+    description =   "high performace python rpc",
+    license     =   "GPLv3",
+    version     =   "0.1.0",
+    author      =   "WEN",
+    author_email=   "test@gmail.com",
+    url         =   "https://github.com/NCNU-OpenSource/testrepo",
+    packages    =   ['easyrpc'],
+    install_requires=[
+        'msgpack',
+        ''
+    ],
+    entry_points='''
+        [console_scripts]
+        easyrpc=easyrpc:easyrpc
+    ''',
+    classifiers=[
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+    ],
+)
